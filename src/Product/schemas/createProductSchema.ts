625c654e226dd370cda8c870dfab09b2534e2eb4
LuisAgustinDucret <>
2024-07-22 09:57:02 -0300

    fix schema

--- src/Product/schemas/createProductSchema.ts
+++ src/Product/schemas/createProductSchema.ts
@@ -2,7 +2,26 @@ import { z } from "zod";
 
 const createProductSchema = z.object({
   description: z.string().min(2, { message: "nameMustBeAtleast3" }),
-  minimumQuantity: z.number(),
+  minimumQuantity: z.string().transform((val, ctx) => {
+    const parsed = Number.parseInt(val.replaceAll(".", ""), 10);
+    if (Number.isNaN(parsed)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Required",
+      });
+
+      return z.NEVER;
+    }
+    if (parsed <= 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "mustBePositive",
+      });
+
+      return z.NEVER;
+    }
+    return parsed;
+  }),
 });
 
 export type CreateProductSchema = z.infer<typeof createProductSchema>;
