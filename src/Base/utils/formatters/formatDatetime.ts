bcee5e85e9cb145c9d79cfef5475d8e356a3dfca
br1giacometti <78874204+br1giacometti@users.noreply.github.com>
2024-08-01 00:54:03 -0300

    formaterDate

--- src/Base/utils/formatters/formatDatetime.ts
+++ src/Base/utils/formatters/formatDatetime.ts
@@ -1,8 +1,22 @@
 import dayjs from "dayjs";
 import "dayjs/locale/es";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 const formatDatetime = (date: Date) => {
-  const formattedDate = dayjs(date).format("DD/MM/YYYY HH:mm");
+  const now = dayjs();
+  const givenDate = dayjs(date);
+
+  if (givenDate.isSame(now, "day")) {
+    return `Hoy a las ${givenDate.format("HH:mm")}`;
+  }
+
+  if (givenDate.isSame(now.subtract(1, "day"), "day")) {
+    return `Ayer a las ${givenDate.format("HH:mm")}`;
+  }
+
+  const formattedDate = givenDate.locale("es").format("DD/MM/YYYY HH:mm");
   return formattedDate;
 };
 
